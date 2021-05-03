1e2d76fb6c306b1c8a66c1f573e00ceb7eda3c00
Pawl <pawl-patrol@protonmail.ch>
2021-05-03 19:26:39 +0200

    Update random.cpp

--- random.cpp
+++ random.cpp
@@ -1,53 +1,39 @@
+#include <fxcg/rtc.h>
+
 #define MODULUS 0x100000000 // 2^32
 #define MULTIPLIER 22695477
 #define INCREMENT 1
 
-class Random
-{
-    private:
-        int seed;
-    public:
-        void init(int time);
-        void next();
-        int int32();
-        short int16();
-        int range(int min, int max);
-        bool boolean();
-};
+int seed = RTC_GetTicks();
 
-void Random::init(int time)
-{
-    seed = time;
-}
-
-void Random::next()
+void next()
 {
     seed = (MULTIPLIER * seed + INCREMENT) % MODULUS;
 }
 
-int Random::int32()
+int int32()
 {
     next();
     return seed;
 }
 
-short Random::int16()
+short int16()
 {
     next();
     return seed >> 16;
 }
 
-int Random::range(int min, int max)
+int range(int min, int max)
 {
     return min + (unsigned short)int16() % (max + 1 - min);
 }
 
-bool Random::boolean()
+bool boolean()
 {
     return range(0, 1);
 }
 
-int Random::sign()
+int sign()
 {
     return 1 - 2 * boolean();
 }
